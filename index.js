4fc126aa392c1956be920f8e7620bc08d74076b6
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-09-03 17:18:54 -0400

    post and first get working

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const port = 5000;
+
+const server = express();
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+  const userInfo = req.body;
+
+  if(userInfo.name && userInfo.bio) {
+    db.insert(userInfo)
+      .then(result => {
+        res.json(result);
+        res.status(201);
+      })
+      .catch(error => {
+        res.rend(error);
+        res.status(500);
+      });
+  } else {
+    res
+      .status(400).json({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(response => {
+      res.json(response);
+      res.status(200);
+    })
+    .then(error => {
+      res.rend(error);
+      res.status(500);
+    });
+})
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
